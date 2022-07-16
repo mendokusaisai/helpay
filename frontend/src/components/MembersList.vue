d2db11322cadf98f7b72242039cf26209482b14e
Kimura Yoshiyuki <mendokusaisai@gmail.com>
2022-07-16 18:28:45 +0900

    member複数選択

--- frontend/src/components/MembersList.vue
+++ frontend/src/components/MembersList.vue
@@ -1,8 +1,15 @@
 <template>
   <v-container>
+    <p>{{ selections }}</p>
     <v-row dense>
       <v-col v-for="member in members" :key="member.id" :cols="6">
-        <v-card>
+        <v-card
+          :color="selections.includes(member.id) ? 'primary' : undefined"
+          :dark="selections.includes(member.id)"
+          class="mx-auto"
+          rounded="xl"
+          @click="changeSelection(member.id)"
+        >
           <v-card-title>{{ member.name }}</v-card-title>
           <v-card-subtitle>{{ member.point }}pt</v-card-subtitle>
         </v-card>
@@ -21,12 +28,23 @@ export default defineComponent({
   data() {
     return {
       members: Array<Member>(),
+      active: false,
+      selections: Array<number>(),
     };
   },
   mounted() {
     axios.get("/api/members/").then((response) => {
       this.members = response["data"];
     });
+  },
+  methods: {
+    changeSelection: function (memberId: number) {
+      if (this.selections.includes(memberId)) {
+        this.selections.splice(this.selections.indexOf(memberId), 1);
+      } else {
+        this.selections.push(memberId);
+      }
+    },
   },
 });
 </script>
